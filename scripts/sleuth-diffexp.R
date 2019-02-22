3d13c75215f310b59195df92b8a617c508f48b7a
jafors <forster_jan@web.de>
2019-02-22 13:06:51 +0100

    Added aggregated gene level output

--- scripts/sleuth-diffexp.R
+++ scripts/sleuth-diffexp.R
@@ -8,7 +8,11 @@ so <- sleuth_fit(so, as.formula(snakemake@params[["model"]]), 'full')
 so <- sleuth_fit(so, ~1, 'reduced')
 so <- sleuth_lrt(so, 'reduced', 'full')
 
-all <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE)
+all <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE, pval_aggregate = FALSE)
 all <- dplyr::arrange(all, pval)
 
+all_aggregated <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE, pval_aggregate = TRUE)
+all_aggregated <- dplyr::arrange(all_aggregated, pval)
+
 write.table(all, file = snakemake@output[[1]], quote=FALSE, sep='\t', row.names = FALSE)
+write.table(all_aggregated, snakemake@output[[2]], quote=FALSE, sep='\t', row.names = FALSE)
