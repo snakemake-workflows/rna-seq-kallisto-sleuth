cf403f57f26427ed32605d6316e09064b89424fb
David Laehnemann <david.laehnemann@hhu.de>
2019-06-04 11:07:58 +0000

    debug sleuth-init.R: specify tab-separator, na.string as empty string and set header = TRUE

--- scripts/sleuth-init.R
+++ scripts/sleuth-init.R
@@ -13,7 +13,7 @@ t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id",
 t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id,
   ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
 
-samples <- read.table(snakemake@input[["samples"]], colClasses = "factor")
+samples <- read.table(snakemake@input[["samples"]], colClasses = "factor", sep = "\t", na.strings = "", header = TRUE)
 samples[, "path"] <- as.character(samples[, "path"])
 
 if(!is.null(model)) {
