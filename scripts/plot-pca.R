13dcc6e5e41ad42895a25500ff52df1a78a1b7fe
David Laehnemann <david.laehnemann@hhu.de>
2019-06-04 15:21:43 +0000

    scripts/plot-pca.R: switch from dots to text labels, to have sample names in the PCA plots

--- scripts/plot-pca.R
+++ scripts/plot-pca.R
@@ -4,5 +4,5 @@ suppressMessages({
 
 so <- sleuth_load(snakemake@input[[1]])
 pdf(file = snakemake@output[[1]])
-plot_pca(so, color_by = snakemake@wildcards[["covariate"]])
+plot_pca(so, color_by = snakemake@wildcards[["covariate"]], text_labels = TRUE)
 dev.off()
