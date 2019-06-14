f04ac3e625dc49f0f311eaf51190c64c417261f8
Johannes Köster <johannes.koester@tu-dortmund.de>
2019-06-14 19:57:54 +0000

    initial work on unsupervised clustering

---
+++ scripts/plot-unsupervised-heatmap.R
@@ -0,0 +1,13 @@
+suppressMessages({
+  library("sleuth")
+})
+
+so <- sleuth_load(snakemake@input[["so"]])
+
+diffexp <- read.table(snakemake@input[["diffexp"]], sep = "\t", header = TRUE)
+
+pdf(file = snakemake@output[[1]], width = 14)
+plot_transcript_heatmap(so, transcripts = diffexp$target_id[1:20]) 
+# TODO, once pachterlab/sleuth#214 is merged, add this to get gene names
+# , labels_row = diffexp$ext_gene[1:20])
+dev.off()
