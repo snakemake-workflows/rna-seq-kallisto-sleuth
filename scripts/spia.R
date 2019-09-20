d2fb3f00052a4a72c62dab1c97070b3b2fcce339
David Laehnemann <david.laehnemann@hhu.de>
2019-09-20 16:54:48 +0000

    scripts/spia.R: minor tidy up for readability

--- scripts/spia.R
+++ scripts/spia.R
@@ -8,38 +8,38 @@ options(Ncpus = snakemake@threads)
 
 covariate <- snakemake@params[["covariate"]]
 
-prefix_ens_ids <- function(ids) {
-    paste("ENSEMBL", ids, sep = ":")
-}
-
 db <- pathways(snakemake@params[["species"]], "reactome")
 db <- convertIdentifiers(db, "ENSEMBL")
 
 prepareSPIA(db, "reactome")
 
 
-diffexp <- read_tsv(snakemake@input[["diffexp"]]) %>% drop_na(ens_gene) %>% mutate(ens_gene = str_c("ENSEMBL", ens_gene))
-universe <- diffexp %>% pull(ens_gene)
-diffexp <- diffexp %>% filter(qval <= 0.05)
+diffexp <- read_tsv(snakemake@input[["diffexp"]]) %>%
+            drop_na(ens_gene) %>%
+            mutate(ens_gene = str_c("ENSEMBL", ens_gene))
+universe <- diffexp %>% pull(var = ens_gene)
+sig_genes <- diffexp %>% filter(qval <= 0.05)
 
 # get logFC equivalent (the sum of beta scores of covariates of interest)
-beta_col <- paste("b", covariate, sep = "_")
+beta_col <- str_c("b", covariate, sep = "_")
 
-cols <- colnames(diffexp)
+cols <- colnames(sig_genes)
 suffixes <- c("", "1", ".0")
 found <- FALSE
 for(suffix in suffixes) {
-    beta_col <- paste0(beta_col, suffix)
+    beta_col <- str_c(beta_col, suffix)
     if(beta_col %in% cols) {
         found <- TRUE
         break
     }
 }
 if(!found) {
-    stop(paste0("Invalid covariate ", covariate, ", not found in diffexp table."))
+    stop(str_c("Invalid covariate ", covariate, ", not found in diffexp table."))
 }
 
-beta <- diffexp %>% select(ens_gene, beta_col) %>% deframe()
+beta <- sig_genes %>%
+            select(ens_gene, !!beta_col) %>%
+            deframe()
 
 res <- runSPIA(de = beta, all = universe, "reactome", plots = TRUE)
 
