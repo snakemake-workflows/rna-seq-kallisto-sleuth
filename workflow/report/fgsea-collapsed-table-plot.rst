8be1c2b688bb9a0013ae752bc5b615377748f80c
Till Hartmann <till.hartmann@udo.edu>
2021-04-30 12:10:53 +0200

    Miscellaneous fixes (#40)
    * handle empty sets + single_end detection
    
    * .
    
    * snakefmt
    
    * add report rst for fgsea collapsed table plot

---
+++ workflow/report/fgsea-collapsed-table-plot.rst
@@ -0,0 +1,2 @@
+**Gene set enrichment table plot** of all gene sets tested by fgsea. The most significant transcript of each gene was determined by sleuth using the model ``{{ snakemake.params.model["full"] }}``.
+
--- workflow/scripts/plot-diffexp-heatmap.R
+++ workflow/scripts/plot-diffexp-heatmap.R
@@ -10,8 +10,5 @@ diffexp <- read.table(snakemake@input[["diffexp"]], sep = "\t", header = TRUE)
 
 pdf(file = snakemake@output[[1]], width = 14)
 
-plot_transcript_heatmap(so, transcripts = diffexp$target_id[1:20], main="Top 20 differentially expressed transcripts (TPM)")
-
-# TODO, once pachterlab/sleuth#214 is merged, add this to get gene names
-# , labels_row = diffexp$ext_gene[1:20])
+plot_transcript_heatmap(so, transcripts = diffexp$target_id[1:20], main="Top 20 differentially expressed transcripts (TPM)", labels_row = diffexp$ext_gene[1:20])
 dev.off()
