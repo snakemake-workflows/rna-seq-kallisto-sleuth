2e5be41aebe825f662c7f53e958f5b467c82dbbe
manuelphilip <manuelphilip91@gmail.com>
2022-08-26 17:25:59 +0200

    modifed script get_3prime-seq.py

--- workflow/scripts/get_3prime-seqs.py
+++ workflow/scripts/get_3prime-seqs.py
@@ -10,26 +10,15 @@ three_prime_output_file = open(snakemake.output[0], "w")
 
 from Bio import SeqIO
 for seq_record in SeqIO.parse(snakemake.input["ref_fasta"], "fasta"):
-#for seq_record in SeqIO.parse("test1.fa", "fasta"):
     transcript_location = seq_record.description.split(" ")[2]
-    #print(seq_record.seq)
-    #polyrem_seq = re.sub('T+.$|A+.$', '', str(seq_record.seq))
-    #print(polyrem_seq)
     strand = transcript_location.split(":")[5]
     if strand == "1":
-        #polyrem_seq = re.sub('T+$|A+$', '', str(seq_record.seq))
         trimmed_seq_postive = seq_record.seq[-read_length:]
-        #trimmed_seq_postive = polyrem_seq[-5:]
-        #print(">",seq_record.id, sep = "")
-        #print(trimmed_seq_postive)
         print(">",seq_record.id, sep = "", file = three_prime_output_file)
         print(trimmed_seq_postive, file = three_prime_output_file)
     elif strand == "-1":
-        #polyrem_seq = re.sub('^T+|^A+', '', str(seq_record.seq))
         trimmed_seq_negative = seq_record.seq[0:read_length]
-        #trimmed_seq_negative = polyrem_seq[0:5]
-        #print(">",seq_record.id, sep = "")
-        #print(trimmed_seq_negative)
         print(">",seq_record.id, sep = "", file = three_prime_output_file)
         print(trimmed_seq_negative, file = three_prime_output_file)
+
 three_prime_output_file.close()
