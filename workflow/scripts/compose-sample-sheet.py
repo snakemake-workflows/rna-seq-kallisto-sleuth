a69eb6258074b8b5d80b9cd09032a6dcb03496c8
Johannes Köster <johannes.koester@uni-due.de>
2022-10-11 13:11:36 +0200

    fix kallisto path assignment

--- workflow/scripts/compose-sample-sheet.py
+++ workflow/scripts/compose-sample-sheet.py
@@ -5,6 +5,6 @@ samples_ = snakemake.params.units[["sample", "unit"]].merge(snakemake.params.sam
 samples_["sample"] = samples_.apply(
     lambda row: "{}-{}".format(row["sample"], row["unit"]), axis=1
 )
-samples_["path"] = snakemake.input.kallisto_output
+samples_["path"] = list(snakemake.input.kallisto_output)
 del samples_["unit"]
 samples_.to_csv(snakemake.output[0], sep="\t", index=False)
