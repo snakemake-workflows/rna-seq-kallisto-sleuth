92ffa8b1a4090e09ffdd9f98e2d72fb53e12f45a
dlaehnemann <david.laehnemann@hhu.de>
2023-08-30 14:09:35 +0200

    create correct SeqRecord after poly-A tail removal

--- workflow/scripts/remove_poly_tails.py
+++ workflow/scripts/remove_poly_tails.py
@@ -1,12 +1,13 @@
 import sys
 import re
 from Bio import SeqIO
+from Bio.Seq import Seq
 
 sys.stderr = open(snakemake.log[0], "w")
 
 with open(snakemake.output[0], "w") as transcript_clean_cdna_fasta:
     for seq_record in SeqIO.parse(snakemake.input["ref_fasta"], "fasta"):
-        seq_record.seq = re.sub("TTTT+$|AAAA+$", "", str(seq_record.seq))
+        seq_record.seq = Seq(re.sub("TTTT+$|AAAA+$", "", str(seq_record.seq)))
         SeqIO.write(
             seq_record,
             transcript_clean_cdna_fasta,
