23c1478addfd4924282543a45a8d8568edf7702d
Addimator <adpri100@hhu.de>
2024-08-01 14:52:43 +0200

    improve go enrichment postprocessing

--- workflow/scripts/postprocess_go_enrichment.py
+++ workflow/scripts/postprocess_go_enrichment.py
@@ -1,14 +1,14 @@
-import pandas as pd
+# import pandas as pd
 from ast import literal_eval as make_tuple
+import polars as pl
 
 
-def calculate_enrichment(ratio_in_study_str, ratio_in_pop_str):
+
+def calculate_enrichment(ratio_str):
     """Calculate enrichment based on ratios provided as strings."""
-    ratio_in_study = make_tuple(ratio_in_study_str)
-    ratio_in_pop = make_tuple(ratio_in_pop_str)
-    enrichment_study = ratio_in_study[0] / ratio_in_study[1]
-    enrichment_pop = ratio_in_pop[0] / ratio_in_pop[1]
-    return enrichment_study, enrichment_pop
+    ratio = make_tuple(ratio_str)
+    enrichment = ratio[0] / ratio[1]
+    return enrichment
 
 
 def sort_group(group):
@@ -17,32 +17,29 @@ def sort_group(group):
 
 
 # Load data
-df_enr = pd.read_csv(snakemake.input["enrichment"], sep='\t')
-df_sig = pd.read_csv(snakemake.input["significant_terms"], sep='\t')
+df_enr = pl.read_csv(snakemake.input["enrichment"], separator='\t')
+df_sig = pl.read_csv(snakemake.input["significant_terms"], separator='\t')
 
-# TODO Would be better with merge but does not work right now
-# # Merge the two dataframes on the 'GO' column to keep only common GO terms
-# df_merged = df_sig.join(df_enr.set_index('GO'), on='GO', rsuffix='_enr')
-# # Add study items from significant terms to the merged dataset
-# df_merged['study_items_sig_terms'] = df_merged['study_items']
-# Only keep data if GO term exists in both tables
-common_ids = df_sig[df_sig['GO'].isin(df_enr['GO'])]['GO']
-df_enr_filtered = df_enr[df_enr['GO'].isin(common_ids)]
-df_sig_filtered = df_sig[df_sig['GO'].isin(common_ids)]
+# We only want to have the study items from df_sig
+df_sig = df_sig.select(["GO", "study_items"])
+df_merged = df_enr.join(df_sig, on="GO", how="inner", suffix="_df2")
+df_merged_sorted = df_merged.with_columns(
+    pl.col("study_items_df2").alias("study_items")
+).drop("study_items_df2")
 
-# Add study items from significant terms to dataset
-df_enr_filtered['study_items_sig_terms'] = df_enr_filtered['GO'].map(
-    df_sig_filtered.set_index('GO')['study_items'])
 
-# Sort and calculate enrichment ratios
-df_merged_sorted = df_enr_filtered.groupby(
-    'class', group_keys=False).apply(sort_group)
-
-if not df_merged_sorted.empty:
-    df_merged_sorted['enrichment'] = df_merged_sorted.apply(
-        lambda row: calculate_enrichment(row['ratio_in_study'], row['ratio_in_pop']), axis=1)
+if not df_merged_sorted.is_empty():
+    df_merged_sorted = df_merged_sorted.with_columns(
+        pl.struct([
+            pl.col("ratio_in_study").map_elements(calculate_enrichment).alias("enrichment_study"),
+            pl.col("ratio_in_pop").map_elements(calculate_enrichment).alias("enrichment_pop")
+        ]).map_elements(lambda row: f"({row['enrichment_study']}, {row['enrichment_pop']})").alias("enrichment")
+    )
+    print(df_merged_sorted.head(3))
 else:
-    df_merged_sorted['enrichment'] = None
+    df_merged_sorted = df_merged_sorted.with_columns(
+        enrichment = pl.lit(None)
+    )
 
 # Save the result to a file
-df_merged_sorted.to_csv(snakemake.output[0], sep='\t', index=False)
+df_merged_sorted.write_csv(snakemake.output[0], separator='\t')
