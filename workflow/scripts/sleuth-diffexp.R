7b229befd23752f3b502b09e28686212690ae881
fxwiegand <fxwiegand@wgdnet.de>
2024-08-20 12:13:23 +0200

    fix: Move signed_pi_value_* columns to the end

--- workflow/scripts/sleuth-diffexp.R
+++ workflow/scripts/sleuth-diffexp.R
@@ -117,6 +117,14 @@ write_results <- function(so, mode, output, output_all) {
 	              # e.g. useful for GSEA ranking
 	              mutate( !!signed_pi_col_name := -log10(pval) * !!sym(beta_col_name) )
       }
+
+      # Reorder the columns so that "signed_pi_value_*" columns are at the end
+      all <- all %>%
+          select(
+            -starts_with("signed_pi_value"),
+            starts_with("signed_pi_value")
+          )
+
       # saving volcano plots
       marrange_volcano <- marrangeGrob(grobs=volcano_list, nrow=1, ncol=1, top = NULL)
       ggsave(snakemake@output[["volcano_plots"]], plot = marrange_volcano, width = 14)
