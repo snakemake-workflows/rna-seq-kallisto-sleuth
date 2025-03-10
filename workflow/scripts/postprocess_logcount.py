407154e1be1e1d3434793fe5d137fd67460111f5
Felix Wiegand <fxwiegand@wgdnet.de>
2025-03-10 15:14:13 +0100

    Update postprocess_logcount.py

--- workflow/scripts/postprocess_logcount.py
+++ workflow/scripts/postprocess_logcount.py
@@ -6,9 +6,9 @@ logcount_matrix = pd.read_csv(snakemake.input['logcount'], sep='\t')
 diffexp = pd.read_csv(snakemake.input['diffexp'], sep='\t')
 
 # Filter logcount_matrix to only include rows where 'transcript' is in 'target_id' of genes_representative
-filtered_logcount_matrix = logcount_matrix[
-    logcount_matrix['transcript'].isin(diffexp['target_id'])
-]
+#filtered_logcount_matrix = logcount_matrix[
+#    logcount_matrix['transcript'].isin(diffexp['target_id'])
+#]
 
 # Save the filtered dataframe to a new TSV file
-filtered_logcount_matrix.to_csv(snakemake.output[0], sep='\t', index=False)
+logcount_matrix.to_csv(snakemake.output[0], sep='\t', index=False)
