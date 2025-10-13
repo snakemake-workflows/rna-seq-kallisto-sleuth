e036131db2b92ab4ee19fcb7d2e18dc926e05fe8
dlaehnemann <1379875+dlaehnemann@users.noreply.github.com>
2025-10-13 19:48:45 +0200

    fix: make boostrap plotting more modern

--- workflow/scripts/plot-bootstrap.R
+++ workflow/scripts/plot-bootstrap.R
@@ -9,50 +9,68 @@ so <- sleuth_load(snakemake@input[["so"]])
 
 top_n <- -strtoi(snakemake@params["top_n"])
 
-results <- read_tsv(snakemake@input[["transcripts"]])
+transcripts <- read_tsv(snakemake@input[["transcripts"]])
 
-top_genes <- results %>%
-   filter(qval <= snakemake@params[["fdr"]]) %>%
-   top_n(top_n, qval) %>%
-   dplyr::select(ext_gene) %>%
-   drop_na() %>%
-   distinct(ext_gene)
-
+top_genes <- transcripts |>
+    filter(qval <= snakemake@params[["fdr"]]) |>
+    top_n(top_n, qval) |>
+    dplyr::select(ext_gene) |>
+    drop_na() |>
+    distinct(ext_gene)
 
 if (snakemake@params[["genes_of_interest"]][["activate"]] == TRUE) {
-  genes = character()
-  for (gene_list in unname(snakemake@params[["genes_of_interest"]][["gene_lists"]])) {
-    new_genes <- read_tsv(
-      gene_list,
-      col_names = FALSE
-    )
-    genes <- union(genes, new_genes |> unlist() |> unname())
-  }
-  genes_of_interest <- enframe(
-    genes,
-    name = NULL,
-    value = "ext_gene"
-  )
+    genes_of_interest <- read_tsv(
+        unlist(unname(snakemake@params[["genes_of_interest"]][["gene_lists"]])),
+        col_names = c("ext_gene")
+    ) |>
+        unique()
 } else {
-  # Create empty tibble when genes_of_interest is not activated
-  genes_of_interest <- tibble(ext_gene = character())
+    # Create empty tibble when genes_of_interest is not activated
+    genes_of_interest <- tibble(ext_gene = character())
 }
-genes <- full_join(top_genes, genes_of_interest, by = "ext_gene") %>%
-    add_row(ext_gene = "Custom") %>%
-    pull(ext_gene)
+
+genes <- full_join(top_genes, genes_of_interest, by = "ext_gene") |>
+    add_row(ext_gene = "Custom")
 
 dir.create(snakemake@output[[1]])
 
-for (gene in genes) {
-    transcripts <- results %>%
-        filter(ext_gene == gene) %>%
-        drop_na() %>%
-        pull(target_id)
+# sleuth seems to do some internal filtering, and bootstrap values are only
+# available for filtered transcripts
+available_transcripts <- so$obs_norm_filt |>
+    select(target_id)
 
-    if (length(transcripts > 0)) {
-        for (transcript in transcripts) {
-            plot_bootstrap(so, transcript, color_by = snakemake@params[["color_by"]], units = "est_counts")
-            ggsave(file = str_c(snakemake@output[[1]], "/", gene, ".", transcript, ".", snakemake@wildcards[["model"]], ".bootstrap.pdf"))
-        }
+transcripts_of_interest <- inner_join(
+    transcripts,
+    genes,
+    by = "ext_gene"
+) |>
+    inner_join(
+        available_transcripts,
+        by = "target_id"
+    ) |>
+    pull(target_id)
+
+
+if (length(transcripts_of_interest > 0)) {
+    for (transcript in transcripts_of_interest) {
+        gene = transcripts |> filter(target_id == transcript) |> pull(ext_gene)
+        plot_bootstrap(
+            so,
+            transcript,
+            color_by = snakemake@params[["color_by"]],
+            units = "est_counts"
+        )
+        ggsave(
+            file = str_c(
+                snakemake@output[[1]],
+                "/",
+                gene,
+                ".",
+                transcript,
+                ".",
+                snakemake@wildcards[["model"]],
+                ".bootstrap.pdf"
+            )
+        )
     }
 }
